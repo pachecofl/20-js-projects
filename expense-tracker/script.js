76d78067736f0a210bc7e4d1da0ef021284f773b
Fernanda <fotografia.flp@gmail.com>
2023-11-17 17:01:55 +0100

    new delete func

--- expense-tracker/script.js
+++ expense-tracker/script.js
@@ -14,22 +14,25 @@ const btnAddTransaction = document.querySelector(".add-transation");
 
 const body = document.querySelector("body");
 
-//Objects
+//Objects and variables
 const expenses = [
   {
     text: "Camera",
     amount: -300,
+    id: 1,
   },
   {
     text: "Salary",
     amount: 2000,
+    id: 2,
   },
   {
     text: "Trip",
     amount: -800,
+    id: 3,
   },
 ];
-
+let itemsNums = 1;
 //Functions
 function calcDisplayBalance() {
   //calculate
@@ -42,21 +45,29 @@ function calcDisplayBalance() {
   const balance = incomeTotal - expenseTotal;
 
   //display
-  totalBalance.textContent = `${balance >= 0 ? "$" : "-$"}${Math.abs(balance)}`;
-  totalIncome.textContent = `$${incomeTotal}`;
-  totalExpense.textContent = `$${expenseTotal}`;
+  totalBalance.textContent = `${balance >= 0 ? "$" : "-$"}${Math.abs(balance).toFixed(2)}`;
+  totalIncome.textContent = `$${incomeTotal.toFixed(2)}`;
+  totalExpense.textContent = `$${expenseTotal.toFixed(2)}`;
 }
 function displayExpenses() {
   expenseList.innerHTML = "";
   expenses.forEach(function (expense) {
+    //add the event listener is possible like that just the way we are addind the other values
     expenseList.innerHTML += `
-    <li>
-      <span class="delete">x</span>
+    <li class="${expense.amount > 0 ? "plus" : "minus"}" value="${expense.id}">
+      <span class="delete" onclick="deleteItem(${expense.id})">x</span>
       <p>${expense.text}</p>
       <p>${expense.amount}</p>
     </li>
     `;
   });
+}
+
+function deleteItem(id) {
+  const expenseIndex = expenses.findIndex((expense) => expense.id === id);
+  expenses.splice(expenseIndex, 1);
+  displayExpenses();
+  calcDisplayBalance();
 }
 
 //Event listeners
@@ -66,26 +77,20 @@ btnAddTransaction.addEventListener("click", function (e) {
   //input values
   const text = textInput.value;
   const amount = Number(amountInput.value);
+  // const id = itemsNums;
+  const id = Number(`${expenses.length >= 1 ? expenses[expenses.length - 1].id + 1 : 1}`);
+  // itemsNums++;
   //check if entry is valid
   if (text && amount) {
     expenses.push({
       text,
       amount,
+      id,
     });
     displayExpenses();
     calcDisplayBalance();
   }
-});
-//delete expense
-body.addEventListener("click", function (e) {
-  if (e.target.className === "delete") {
-    const expenseIndex = expenses.findIndex(
-      (expense) => expense.text === e.target.nextElementSibling.textContent
-    );
-    expenses.splice(expenseIndex, 1);
-    console.log(expenseIndex, expenses);
-    e.target.parentElement.remove();
-  }
+  textInput.value = amountInput.value = "";
 });
 
 //Init
---
+++ expense-tracker/text.md
@@ -0,0 +1,10 @@
+### Bugs
+
+- [x] Income, expense and balance are not updating when you delete something
+- [x] If user has 2 items with same name, the first one will be deleted from the array : add a id x
+
+### To do
+
+- [x] make the delete button only appers when hovering
+- [x] add red and green border depending on value
+- [x] fix value formating .00
